d3a8c1517544a896f5fb1960de2b84217c7bee8b
Ian Ulanday <ulanday.ian@gmail.com>
2021-02-23 17:03:27 -0700

    created includes, added functions for printing and writing can packets to ecu for throttle

---
+++ ecucan.cc
@@ -0,0 +1,23 @@
+#include "ecucan.hh"
+
+CAN_message_t ecu_message;
+
+void printRaw(CAN_message_t ecu_message) {
+    Serial.print(ecu_message.id); Serial.print(" ");
+    for (int i = 0; i < 6; i++) {
+            Serial.print(ecu_message.buf[i], HEX); Serial.print(" ");
+        }
+    Serial.println(" ");
+}
+
+void genericECUWrite(int commandId, uint16_t ecuAddress, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+    ecu_message.flags.extended = true;
+    ecu_message.id = 0x09000000 | (commandId << 6) | ecuAddress;
+    can->write(ecu_message);
+}
+
+void ThrottleCmd(uint8_t throttlePosition, uint16_t ecuAddress, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16>* can) {
+    ecu_message.buf[0] = throttlePosition;
+    ecu_message.buf[1] = 0;
+    genericECUWrite(0x06, ecuAddress, can);
+}
